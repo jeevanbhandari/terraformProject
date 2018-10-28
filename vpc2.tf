d1edc1c9648b3dae6dc836601a3db45891e415b5
jeevanbhandari <bhandarijeevan89@gmail.com>
2018-10-28 20:53:27 +0000

    fixed

---
+++ vpc2.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "main" {
+	cidr_block = "10.0.0.0/16"
+}
+resource "aws_subnet" "artemis-private" {
+	vpc_id = "${aws_vpc.main.id}"         
+	cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "artemis-public" {
+	vpc_id = "${aws_vpc.main.id}"
+	cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_internet_gateway" "artemis" {
+	vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route_table" "artemis" {
+        vpc_id = "{aws_vpc.main.id}"
+
+
+	route {
+	   cidr_block  = "0.0.0.0/0"
+	   gateway_id = "${aws_internet_gateway.artemis.id}"
+  }
+}
+
