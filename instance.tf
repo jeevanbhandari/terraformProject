58433568afd49aa166b8757f59962d5ba939772a
jeevanbhandari <bhandarijeevan89@gmail.com>
2018-11-01 02:34:44 +0000

    keyadded

--- instance.tf
+++ instance.tf
@@ -13,6 +13,11 @@ resource "aws_instance" "instance1" {
     Environment = "${var.Environment}"
     Created_by  = "${var.Created_by}"
   }
+
+  lifecycle {
+    prevent_destroy = "true"
+
+   }
 }
 
 # DB Host
@@ -30,4 +35,8 @@ resource "aws_instance" "dbhost" {
     Environment = "${var.Environment}"
     Created_by  = "${var.Created_by}"
   }
+
+  lifecycle {
+   prevent_destroy = "true"
+ }
 }
--- keys.tf
+++ keys.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "project1" {
   key_name   = "project1"
-  public_key = "${file("/root/.ssh/id_rsa.pub")}"
+  public_key = "${file("/var/lib/jenkins/.ssh/id_rsa.pub")}"
 }
